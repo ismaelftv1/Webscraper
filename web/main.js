372debe3bd63cf23b21cead24053c6bbb9383bc2
ismael M <ismaelmontelongomon@gmail.com>
2023-05-16 11:30:08 +0100

    Add: u can see the discount in the web

--- web/main.js
+++ web/main.js
@@ -15,9 +15,10 @@ function RellenarTabla(datos = []) {
         precio = parseFloat(datos[i].Precio_Sin_IVA);
         igic = precio * 1.07;
         total += igic;
+        let descuento = datos[i].descuento;;
 
         //rellena la tabla con los datos de productos.json
-        texto.innerHTML += `<tr><td>${datos[i].name}</td><td class="precio">${precio}€</td><td class="precio">${igic.toFixed(2)}€</td></tr>`;
+        texto.innerHTML += `<tr><td>${datos[i].name}</td><td class="precio">${precio}€</td><td class="precio">${igic.toFixed(2)}€</td><td class="precio">${descuento}</td></tr>`;
     }
     texto.innerHTML += `<tr><th colspan="2" class="total">Total:</th><td class ="precio">${total.toFixed(2)}€</td></tr>`;
 }
